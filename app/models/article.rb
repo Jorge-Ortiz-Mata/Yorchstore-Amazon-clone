691ff289cf255e630086708c4f37f885cfc92c5d
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-10 01:10:53 -0600

    Google Cloud Storage configuration.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    has_many_attached :images
     after_create_commit {broadcast_prepend_to "articles"}
     after_update_commit {broadcast_replace_to "articles"}
     after_destroy_commit {broadcast_remove_to "articles"}
