7ea69f3050a2306f566381226c6f7d73d3234b6f
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-12 18:27:01 -0600

    Add item working.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,11 +5,16 @@ class OrdersController < ApplicationController
     before_action :user_profile
 
     def create 
-        @article = Article.look_up(params[:article], params[:quantity])
-        @quantity_request = params[:quantity]
+        @article = Article.find(params[:article].to_i)
+        @quantity_request = params[:quantity].to_i
         if @article.present? && @quantity_request.to_i > 0
             if @quantity_request.to_i <= @article.stock
-                redirect_to @article, notice: "Article added to your car."
+                @order = Order.new(user_id: current_user.id, article_id: @article.id, quantity: @quantity_request, product_title: @article.title, product_description: @article.description)
+                if @order.save
+                    @var_x = @article.stock - @quantity_request
+                    @article.update(stock: @var_x)
+                    redirect_to @article, notice: "Article added to your car. #{@var_x}"
+                end
             else
                 redirect_to @article, alert: "There aren't many items in stock. Come back later."
             end
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,9 +5,4 @@ class Article < ApplicationRecord
     after_create_commit {broadcast_prepend_to "articles"}
     after_update_commit {broadcast_replace_to "articles"}
     after_destroy_commit {broadcast_remove_to "articles"}
-
-    def self.look_up(article_id, quantity)
-        article = find(article_id.to_i)
-    end
-
 end
