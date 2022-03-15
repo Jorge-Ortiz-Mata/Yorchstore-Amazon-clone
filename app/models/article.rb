887dab3ce9c49637c66a715ef41068b5aee557f6
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-15 00:46:22 -0600

    Adding styling.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,11 @@ class Article < ApplicationRecord
     belongs_to :user
     has_many_attached :images, dependent: :destroy
     has_many :orders
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
+    validates :user_id, presence: true
+    validates :stock, presence: true
     after_create_commit {broadcast_prepend_to "articles"}
     after_update_commit {broadcast_replace_to "articles"}
     after_destroy_commit {broadcast_remove_to "articles"}
